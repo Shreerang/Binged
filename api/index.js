b592d4515a972a75a7906b7ed26fd28dac299dba
Shreerang Patwardhan <spatwardhan@wsgc.com>
2019-12-17 15:23:43 -0800

    Trying to resolve the Heroku H12 issue

--- api/index.js
+++ api/index.js
@@ -25,6 +25,60 @@ fs.createReadStream(__dirname + '/tv_ids.json')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const extendTimeoutMiddleware = (req, res, next) => {
+  const space = ' '
+  let isFinished = false
+  let isDataSent = false
+
+  // Only extend the timeout for API requests
+  if (!req.url.includes('/api')) {
+    next()
+    return
+  }
+
+  res.once('finish', () => {
+    isFinished = true
+  })
+
+  res.once('end', () => {
+    isFinished = true
+  })
+
+  res.once('close', () => {
+    isFinished = true
+  })
+
+  res.on('data', data => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true
+    }
+  })
+
+  const waitAndSend = () => {
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202)
+        }
+
+        res.write(space)
+
+        // Wait another 15 seconds
+        waitAndSend()
+      }
+    }, 15000)
+  }
+
+  waitAndSend()
+  next()
+}
+
+app.use(extendTimeoutMiddleware)
+
 app.post('/upload', upload.single('myfile'), (req, res, next) => {
   const csv = require('csv-parser')
   const results = []
@@ -125,7 +179,7 @@ async function fetchDetails(array) {
   const axios = require('axios')
   const rateLimit = require('axios-rate-limit')
   const http = rateLimit(axios.create(), {
-    maxRequests: 3,
+    maxRequests: 4,
     perMilliseconds: 1000
   })
   http.getMaxRPS()
